4ced0490f4a2997ff23ffce0ac038c9a1b922c94
Marcos <marcos@Marcoss-MacBook-Pro-3.local>
2020-09-26 19:28:22 -0300

    4 algos dones

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,25 @@
 function addTwoDigits(n: any): number {
+  const nums = n.toString().split('');
+
+    return nums.reduce((a: string, b: string) => {
+        return parseInt(a) + parseInt(b);
+    });
 
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
+
+
+
+
+
+
+
+
+function addTwoDigits2(n: any): number {
+  const nums = n.toString().split('');
+
+    return parseInt(nums[0]) + parseInt(nums[1]);
+}
+
+console.log(addTwoDigits2(29));
