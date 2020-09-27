4a855a713985087be1e9f5c515871daee4dfe56c
Marcos <marcos@Marcoss-MacBook-Pro-3.local>
2020-09-27 02:53:59 -0300

    update

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,14 @@
 function adjacentElementsProduct(inputArray: number[]): number {
+    let largestProduct = inputArray[0] * inputArray[1];
+
+    for (let i = 1; i < inputArray.length - 1; i++) {
+
+        const total = inputArray[i] * inputArray[i + 1];
+
+        largestProduct = largestProduct < total ? total : largestProduct;
+    }
+    return largestProduct;
 
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
