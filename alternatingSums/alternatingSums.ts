6eda76b7d7fca484e599691c198dc36aa9ac8e28
Marcos <marcos@Marcoss-MacBook-Pro-3.local>
2020-10-04 16:36:25 -0300

    update

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,4 +1,17 @@
-function alternatingSums(a: number[]): number[] {
+function alternatingSums(persons: number[]): number[] {
+
+    let teamOne = 0;
+    let teamTwo = 0;
+
+    persons.forEach((element, index) => {
+        if (index % 2 === 0) {
+            teamOne += element;
+        } else {
+            teamTwo += element;
+        }
+    });
+
+        return [teamOne, teamTwo];
 
 }
 
--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,15 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+    const yourWeakest = yourLeft <= yourRight ? yourLeft : yourRight;
+    const yourStrongest = yourLeft >= yourRight ? yourLeft : yourRight;
+    const friendWeakest = friendsLeft <= friendsRight ? friendsLeft : friendsRight;
+    const friendStrongest = friendsLeft >= friendsRight ? friendsLeft : friendsRight;
+
+    return yourStrongest === friendStrongest && yourWeakest === friendWeakest;
+
+
 
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
