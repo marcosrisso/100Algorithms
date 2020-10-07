af83b238bb0196ff6dc46452fb87f2e3c5490bae
Marcos <marcos@Marcoss-MacBook-Pro-3.local>
2020-10-07 01:35:34 -0300

    update

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,29 @@
 function arrayConversion(inputArray: number[]): number {
+    let isOdd = true;
 
+    while(inputArray.length !== 1) {
+        inputArray = sumProduct(inputArray, isOdd);
+        isOdd = !isOdd;
+    }
+
+    return inputArray[0];
+
+}
+
+function sumProduct(nums: number[], isOdd: boolean): number[] {
+    const products: number[] = [];
+
+    if (isOdd) {
+        for (let i = 0; i < nums.length; i += 2) {
+            products.push(nums[i] + nums[i + 1]);
+
+        }
+    } else {
+        for (let i = 0; i < nums.length; i += 2) {
+            products.push(nums[i] * nums[i + 1]);
+        }
+    }
+    return products;
 }
 
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
