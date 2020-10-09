91664a324904cb0105baf3581fd74371856ab34f
Marcos MacBook <marcosrisso@me.com>
2020-10-09 20:09:36 -0300

    update

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,23 @@
-function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+function arrayMaxConsecutiveSum(inputArray: number[], count: number): number {
 
+        let sum = 0;
+        let maxResult = 0;
+
+        for ( let i = 0; i < count; i++ ) {
+            sum += inputArray[i];
+        }
+
+        maxResult = sum;
+
+        for ( let i = count; i < inputArray.length; i++ ) {
+            sum -= inputArray[i - count];
+            sum += inputArray[i];
+        
+                if ( sum > maxResult ) {
+                    maxResult = sum;
+                }
+        }
+        return maxResult;
 }
 
 console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,31 @@
 function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+    let maxDiff = Math.abs(inputArray[0] - inputArray[1]);
+
+    for ( let i = 0; i < inputArray.length; i++ ) {
+        let absoluteDiff = Math.abs(inputArray[i - 1] - inputArray[i]);
+        maxDiff = absoluteDiff > maxDiff ? absoluteDiff : maxDiff;
+    }
+
+        return maxDiff;
 
 }
 
 console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+
+
+
+// alternative solution
+
+function arrayMaximalAdjacentDifference2(inputArray: number[]): number {
+
+	var max = -1;
+    var temp;
+	for (var i = 0; i < inputArray.length - 1; i++)
+      {
+		temp = Math.abs(inputArray[i] - inputArray[i + 1]);
+		max = Math.max(max, temp);
+	  }
+	return max;
+}
+
+console.log(arrayMaximalAdjacentDifference2([2, 4, 1, 0]));
