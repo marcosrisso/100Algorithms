951f76ea9d60d90aea17a01cda57b382a05bd971
Marcos MacBook <marcosrisso@me.com>
2020-10-23 01:33:52 -0300

    update

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,18 @@
 function arrayPreviousLess(items: number[]): number[] {
 
+        const findResult = [];
+        
+            for ( let i = items.length - 1;  i >= 0; i-- ) {
+                for ( let x = i; x >= 0; x-- ) {
+                    if ( items[i] > items[x] ) {
+                        findResult.unshift(items[x]);
+                        break;
+                    } else if ( x === 0 ) {
+                        findResult.unshift(-1);
+                    }
+                }
+            }
+        return findResult;
 }
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
